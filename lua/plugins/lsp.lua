88caad44b2f6c368da63a72539c5907493b124b1
Tom Ford <tomfordweb@gmail.com>
2025-09-15 06:32:40 -0400

    decrease timeout

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -20,7 +20,7 @@ return {
           },
           format_on_save = {
             -- These options will be passed to conform.format()
-            timeout_ms = 500,
+            timeout_ms = 200,
             lsp_format = "fallback",
           },
         })
