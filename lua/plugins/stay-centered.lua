9cc4d34805e339ab23588b95d9bbee90175ea4bb
Tom Ford <tomfordweb@gmail.com>
2025-09-29 06:22:36 -0400

    oooooh

--- lua/plugins/stay-centered.lua
+++ lua/plugins/stay-centered.lua
@@ -5,7 +5,7 @@ return {
     require('stay-centered').setup({
       -- The filetype is determined by the vim filetype, not the file extension. In order to get the filetype, open a file and run the command:
       -- :lua print(vim.bo.filetype)
-      skip_filetypes = {},
+      skip_filetypes = { "alpha" },
       -- Set to false to disable by default
       enabled = true,
       -- allows scrolling to move the cursor without centering, default recommended
