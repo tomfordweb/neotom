a5315378a23b3ae819ffec82791b1263d3ab4c6d
Tom Ford <tomfordweb@gmail.com>
2025-10-03 06:19:32 -0400

    catpuccin

--- lua/neotom/init.lua
+++ lua/neotom/init.lua
@@ -6,7 +6,9 @@ require("neotom.lotto-text");
 
 
 function SetColors(color)
-  color = color or "tokyonight-night"
+  -- color = color or "catppuccin-macchiato" -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+  color = color or "catppuccin-mocha" -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+  -- color = color or "tokyonight-night"
   vim.cmd.colorscheme(color)
   -- https://neovim.io/doc/user/syntax.html#_13.-highlight-command
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
