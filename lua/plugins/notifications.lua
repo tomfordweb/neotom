7d4a2f97e9562dd5368739b970b95d451dcdaf96
Tom Ford <tom@datacompusa.com>
2025-11-24 06:04:27 -0500

    snack notifications
    wqq

---
+++ lua/plugins/notifications.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    notifier = { enabled = true },     -- Ensure this is set to true
+    -- Other snacks.nvim options
+  },
+}
