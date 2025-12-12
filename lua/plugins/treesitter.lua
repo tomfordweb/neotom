322ef5c0413f724945484bc9bb83b40bb9b43e42
Tom Ford <tom@datacompusa.com>
2025-12-12 07:59:43 -0500

    angular tree sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,13 +2,14 @@ return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
     "nvim-treesitter/nvim-treesitter-context",
+    "dlvandenberg/tree-sitter-angular",
     'brenoprata10/nvim-highlight-colors'
   },
   branch = 'master',
   lazy = false,
   build = ":TSUpdate",
   config = function()
-    require('nvim-highlight-colors').setup();
+    require('nvim-highlight-colors').setup({});
     require('nvim-treesitter.configs').setup {
       -- A list of parser names, or "all" (the listed parsers MUST always be installed)
       ensure_installed = { "angular", "html", "css", "lua", "vim", "vimdoc", "typescript", "javascript", "markdown", "markdown_inline" },
