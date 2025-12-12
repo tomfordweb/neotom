a12451a5abc160b9940cfe1d738c042aec32b1df
Tom Ford <tom@datacompusa.com>
2025-12-12 07:59:51 -0500

    format scripts

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,6 +7,8 @@ return {
       config = function()
         require("conform").setup({
           formatters_by_ft = {
+            sh = { "shfmt" },
+            bash = { "shfmt" },
             javascript = { "prettierd" },
             typescript = { "prettierd" },
             html = { "prettierd" },
