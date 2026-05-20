a610ae9487d8763ce4ab764f57828ca76472404f
Tom Ford <tomfordweb@gmail.com>
2026-05-20 06:06:38 -0400

    rice

--- lua/plugins/start-screen.lua
+++ lua/plugins/start-screen.lua
@@ -12,32 +12,32 @@ return {
   config = function()
     local startify = require("alpha.themes.startify")
 
-    local function multi_line_text_to_table(result)
-      local pos, fonttbl = 0, {}
-      for st, sp in function() return string.find(result, "\n", pos, true) end do
-        table.insert(fonttbl, string.sub(result, pos, st - 1))
-        pos = sp + 1
-      end
-      table.insert(fonttbl, string.sub(result, pos))
-      return fonttbl
-    end
-
-    local function mergeTables(destinationTable, sourceTable)
-      for i = 1, #sourceTable do
-        table.insert(destinationTable, sourceTable[i])
-      end
-      return destinationTable
-    end
 
     startify.file_icons.provider = "devicons"
 
-    startify.section.header.val = mergeTables(
-      multi_line_text_to_table(
-        vim.fn.system('jp2a  $XDG_CONFIG_HOME/nvim/neotom.png --invert --width=80')
-      ), {
-        "nvim Version " .. vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch,
-        "neotom version " .. vim.fn.system("git rev-parse --short HEAD"):gsub("%s+$", ""),
-      })
+    startify.section.header.val = {
+      "                @@@  C@@@   @@@G@@@    @@@@    t@0G@@@@@@    @@@t     i@@@,   @@@@@                ",
+      "               @@@@@ @@@@@ @@@@@@@@   O@@@@@@  @@@@@@@@@@   @@@@@@   @@@@@@  @@@@@@                ",
+      "               @@@@@ 8@@@; @@@@@@@@  @@@@@@@@  @@@@@@@@@@  @@@@@@@@  @@@@@@  @@@@@@                ",
+      "               @@@@@@@@@@  @@@@@@@@  @@@@@@@@@ @@@@@@@@@@ @@@@@@@@@  @@@@@@@ @@@@@@                ",
+      "               @@@@@@@@@@  @@@@@@@@  @@@@@@@@@ @@@@@@@@@@ @@@@@@@@@1 @@@@@@@ @@@@@@                ",
+      "               @@@@@@@@@@  @@@@@@@@ 1@@@@@@@@@ @@@@@@@8@@ @@@@@@@@@C 0@@@@@@@@@@@@@                ",
+      "               @@@@@@@@@@  @@@@   @ ,@@@@ @@@@ @@t@@@@ @@ @@@@ C@@@: @@@@@@@@@@@@@@                ",
+      "               @@@@@@@@@@  @@@C     i@@@  @@@@  . @@@@    @@@@  @@@@ @@@@8@@@@1@@@@                ",
+      "               @@@@@@@@@@  @@@@@@.  @@@@@ @@@@    @@@@    @@@@  @@@@ @@@@G@@@@ @@@O                ",
+      "                @@@@@@@@@  @@@@@@@  @@@@@ @@@@    @@@@    @@@@  @@@: @@@@@@@@@ @@@0                ",
+      "                @@@ @@@@@  @@@@@@@  @@@@@ @@@@    @@@@    @@@@  @@@: :@@@@@@@@f@@@L                ",
+      "                @@@ t@@@@  @@@@@@L   @@@@ @@@@    @@@@.    @@@ :@@@t  @@@@ @@@f@@@C                ",
+      "                @@@  @@@@  @@@@      @@@@ @@@@    @@@@     @@@@@@@@:  @@@@ @@  @@@                 ",
+      "                @@@  @@@@  @@@@  @@   @@@@@@@L    @@@@     @@@@@@@@   @@@  @@  @@@                 ",
+      "                @@@   @@@  @@@@@@@@   @@@@@@@      @@@     @@@@@@@    @@@   '  @@@                 ",
+      "                @@@   @@O   @@@@@@@   @@@@@@       @@@     f@@@@@@    @@@      @@@                 ",
+      "                 @@    @    @@@@@@.    @@@@        @@       C@@@@      @@      @@@                 ",
+      "                              @@       .@@                   @@@                @                  ",
+      "                                                                                                   ",
+      "nvim Version " .. vim.version().major .. "." .. vim.version().minor .. "." .. vim.version().patch,
+      "neotom version " .. vim.fn.system("git rev-parse --short HEAD"):gsub("%s+$", ""),
+    }
     require("alpha").setup(
       startify.config
     )
