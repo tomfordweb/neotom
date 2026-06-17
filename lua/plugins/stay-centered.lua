037f80a8203936d7f3b3c8143973e05ddc2af5b5
Tom Ford <tomfordweb@gmail.com>
2026-06-17 05:34:57 -0400

    scrolling hell

--- lua/plugins/stay-centered.lua
+++ lua/plugins/stay-centered.lua
@@ -9,7 +9,7 @@ return {
       -- Set to false to disable by default
       enabled = true,
       -- allows scrolling to move the cursor without centering, default recommended
-      allow_scroll_move = false,
+      allow_scroll_move = true,
       -- temporarily disables plugin on left-mouse down, allows natural mouse selection
       -- try disabling if plugin causes lag, function uses vim.on_key
       disable_on_mouse = true,
